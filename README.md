a22e41b0bb7da1c4f7b8307c203c9e3117ca532c
capsule_man <guillaume.vagner@supelec.fr>
2018-10-08 18:22:16 +0200

    ajout authentification API Objenious - API EasyDrive

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Usable road :
 POST /push to post data  
 GET /push to get all the data  
 GET /push/:type to get data typed (join, uplink, downlink)  
+
+# Installation
+```
+#install nodejs
+sudo apt install nodejs-legacy  
+#install mongoDB (https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/)
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4  
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list  
+sudo apt-get update  
+sudo apt-get install -y mongodb-org  
+```
--- routes/push.js
+++ routes/push.js
@@ -1,29 +1,46 @@
 var express = require('express');
 var router = express.Router();
 var Data = require('../models/Data');
+var config = require('../config');
 
 
 router.post('/', function (req, res, next) {
-    console.log(req.body);
-    Data.create(req.body, function (err, data) {
-        if (err) return next(err);
-        res.render('resp', { message: '200 OK' });
-    })
+    if (req.headers['keyhead'] == config.cred.keyhead) {
+        Data.create(req.body, function (err, data) {
+            if (err) return next(err);
+            res.status(200);
+            res.render('resp', { message: '200 OK' });
+        })    
+    } else {
+        res.status(401);
+        res.render('resp', { message: '401 Unauthorized' });
+    }
 });
 
 router.get('/', function(req, res, next) {
-    Data.find(function (err, data) {
-        if (err) return next(err);
-        res.json(data);
-    });
-  });
+    if (req.headers['keyhead'] == config.cred.getmdp) {
+        Data.find(function (err, data) {
+            if (err) return next(err);
+            res.status(200);
+            res.json(data);
+        });
+    } else {
+        res.status(401);
+        res.render('resp', { message: '401 Unauthorized' });
+    };
+});
   
 router.get('/:type', function(req, res, next) {
-    Data.find({type: req.params.type}, function (err, data) {
-        if (err) return next(err);
-        console.log(req);
-        res.json(data);
-    });
-  });
+    if (req.headers['keyhead'] == config.cred.getmdp) {
+        Data.find({type: req.params.type}, function (err, data) {
+            if (err) return next(err);
+            console.log(req);
+            res.json(data);
+        });
+    } else {
+        res.status(401);
+        res.render('resp', { message: '401 Unauthorized' });
+    }
+});
   
 module.exports = router;
