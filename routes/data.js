14ad3c4253713c8ff8c8ddc5da58846ad37ad357
Guillaume Vagner <guillaume.vagner@supelec.fr>
2019-01-02 16:50:26 +0100

    get data by id

--- routes/data.js
+++ routes/data.js
@@ -13,6 +13,22 @@ router.get('/', VerifyAdmin, function(req, res) {
     .catch(_ => {return res.status(500).send('Internal error.')})
 });
 
+router.get('/:id', VerifyToken, function(req, res) {
+    Promise.all([
+        User.findById(req.userId).exec(),
+        Data.findById(req.params.id).exec()
+    ])
+    .catch(_ => {return res.status(404).send('This data does not exist.')})
+    .then(result => {
+        [user, data] = result;
+        if (!user.admin) return res.status(401).send('You must be admin of the data.');
+            res.status(200).json(data);
+    })
+    .catch(_ => {return res.status(500).send('There was a problem getting the data.')})
+});
+
+
+
 router.get('/length', VerifyAdmin, function(req, res) {
     if (!req.isAdmin) return res.status(401).send('Only for admin team.');
     Data.countDocuments().exec()
